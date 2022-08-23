197297066cf8d4319373dc0896d551d84640cdc1
KianPardoe <kpardoecolli@student.unimelb.edu.au>
2022-08-23 10:42:07 +1000

    Added Test servos arduino

---
+++ Arduino/testMotors/testMotors.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+
+#define SERVO1 14
+#define SERVO2 13
+#define SERVO3 12
+#define SERVO4 9
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+void setup() {
+
+  servo1.attach(SERVO1);
+  servo2.attach(SERVO2);
+  servo3.attach(SERVO3);
+  servo4.attach(SERVO4);
+
+  servo1.write(0)
+  servo2.write(0)
+  servo3.write(0)
+  servo4.write(0)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
