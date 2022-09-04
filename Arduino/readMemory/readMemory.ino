2a63e189e9b8a93e6f87ad15f07b448fad25f382
Evano-Benjamin <e.benjamin.n@gmail.com>
2022-09-04 13:58:41 +1000

    maybe this

--- Arduino/readMemory/readMemory.ino
+++ Arduino/readMemory/readMemory.ino
@@ -30,10 +30,10 @@
 #define SWEEP_SIZE 1
 #define DEG2RAD PI/180.0
 
-void setUpMemory();
+void setupMemory();
 void clearMemory();
 void writeToMemory(String toWrite);
-void readMemory();
+void readMemory(String toRead);
 void writeBaro();
 void writeIMU();
 
@@ -71,10 +71,23 @@ float cumaApogeeError = 0;
 float changeApogeeError = 0;
 
 // Memory
-using namespace mbed;
 QSPIFBlockDevice root(PD_11, PD_12, PF_7, PD_13,  PF_10, PG_6, QSPIF_POLARITY_MODE_1, 40000000);
 MBRBlockDevice blockDevice(&root, 1); 
 int memoryCursor = 0;
+
+/****************************************************/
+// Sensor Declerations
+/****************************************************/
+// BNO055
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+// id, address.
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+
+// BMP390
+Adafruit_BMP3XX bmp;
+float groundLevelPressurehPa = 1013.25;  // (hPa) Gets changed on setup to current pressure val
+
+/****************************************************/
 
 void setup() {
   /****************************************************/
@@ -89,12 +102,80 @@ void setup() {
   my_servo3.attach(SERVO3);
   my_servo4.attach(SERVO4);
   
+  my_servo1.write(0);
+  my_servo2.write(0);
+  my_servo3.write(0);
+  my_servo4.write(0);
+
   // Use built in LED for indicating error
   pinMode(LED_BUILTIN, OUTPUT);
   bool Error_LED = false;
 
   // Use Buzzer to indicate calibration complete
   pinMode(BUZZ_PIN, OUTPUT);
+
+  /****************************************************/
+  // Sensor Setup
+  /****************************************************/
+  // BNO055
+  /* Initialise the sensor */
+  if(!bno.begin()){
+    /* There was a problem detecting the BNO055 ... check your connections */
+    while(1){
+      digitalWrite(LED_BUILTIN, Error_LED);
+      Error_LED = !Error_LED;
+      delay(500);
+    }
+  }
+  
+  bno.setExtCrystalUse(true);
+
+  // BMP390
+  /* Initialise the sensor */
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+    while(1){
+      digitalWrite(LED_BUILTIN, Error_LED);
+      Error_LED = !Error_LED;
+      delay(500);
+    }
+  }
+  
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+  // Sensor Calibration BMP390
+  // Average 10 readings over 5 seconds to set as ground level
+  /* Take 100 readings to get through initial eronious readings*/
+  for(int i=0; i<100; i++){
+  bmp.readPressure();
+  }
+  float pressureSum = 0;
+  for(int i=0; i<10; i++){
+    pressureSum += bmp.readPressure()/100.0F;
+    delay(100);
+  }
+  groundLevelPressurehPa = pressureSum/10.0;
+
+  // Sensor Calibration BNO055
+  // Wait for Calibration and Confirm with 1 second Buzz
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  while(1){
+    if(system < 1 || gyro < 1 || accel < 1 || mag < 1){
+      delay(500);
+      bno.getCalibration(&system, &gyro, &accel, &mag);
+    }else{
+      digitalWrite(BUZZ_PIN, HIGH);
+      delay(1000);
+      digitalWrite(BUZZ_PIN, LOW);
+      break;
+    }
+  }
 
   setUpMemory();
   readMemory();
@@ -103,6 +184,12 @@ void setup() {
 }
 
 void loop() {
+  getAltitude();
+  getIMU();
+  updateApogee(1);
+  updateApogeeErrors();
+  updateFinAngles(1);
+  writeFinAngles();
 
   /****************************************************/
   // Debug Code
@@ -122,7 +209,7 @@ void loop() {
   /****************************************************/
 }
 
-void setUpMemory(){ 
+void setupMemory(){ 
 
   if(blockDevice.init() != 0 || blockDevice.size() != BLOCK_DEVICE_SIZE) {    
     Serial.println("Partitioning block device...");
@@ -134,11 +221,9 @@ void setUpMemory(){
 
 }
 
-void readMemory(){
+void readMemory(String toRead){
 
-    const auto size = blockDevice.get_read_size();
-    char buffer[size] {};
-    blockDevice.read(buffer, 0,size);
+    blockDevice.read(buffer, 0, blockDevice.get_read_size());
     Serial.println(buffer);
     FILE * fPtr;    
     fPtr = fopen("log.txt", "w");
@@ -153,9 +238,7 @@ void readMemory(){
 }
 
 void writeToMemory(String toWrite){
-
-  const auto eraseBlockSize = blockDevice.get_erase_size();
-  const auto programBlockSize = blockDevice.get_program_size();
+  
   const auto messageSize = toWrite.length() + 1;
   const unsigned int requiredEraseBlocks = ceil(messageSize / (float)  eraseBlockSize);
   const unsigned int requiredBlocks = ceil(messageSize / (float)  programBlockSize);
@@ -168,27 +251,29 @@ void writeToMemory(String toWrite){
 
 void writeBaro(){
 
-  String toWrite = "Altitude (Baro): " + String(rocketPos[2]);
-  toWrite = toWrite + "\nZ-Velocity (Baro): " + String(rocketVel[2]);
+  String toWrite = 'Altitude (Baro): ' + String(rocketPos[2])
+  toWrite = toWrite + '\nZ-Velocity (Baro): ' + String(rocketVel[2])
   writeToMemory(toWrite);
 
 }
 void writeIMU(){
 
-  String toWrite = "X-Ang: " + String(rocketAngPos[0]);
-  toWrite = toWrite + "\nY-Ang: " + String(rocketAngPos[1]);
-  toWrite = toWrite + "\nZ-Ang: " + String(rocketAngPos[2]);
-  toWrite = toWrite + "\nY-Accel: " + String(rocketAcc[0]);
-  toWrite = toWrite + "\nY-Accel: " + String(rocketAcc[1]);
-  toWrite = toWrite + "\nZ-Accel: " + String(rocketAcc[2]);
-  toWrite = toWrite + "\nX-AngVel: " + String(rocketAngVel[0]);
-  toWrite = toWrite + "\nY-AngVel: " + String(rocketAngVel[1]);
-  toWrite = toWrite + "\nZ-AngVel: " + String(rocketAngVel[2]);
+  String toWrite = 'X-Ang: ' + String(rocketAngPos[0])
+  toWrite = toWrite + '\nY-Ang: ' + String(rocketAngPos[1])
+  toWrite = toWrite + '\nZ-Ang: ' + String(rocketAngPos[2])
+  toWrite = toWrite + '\nY-Accel: ' + String(rocketAcc[0])
+  toWrite = toWrite + '\nY-Accel: ' + String(rocketAcc[1])
+  toWrite = toWrite + '\nZ-Accel: ' + String(rocketAcc[2])
+  toWrite = toWrite + '\nX-AngVel: ' + String(rocketAngVel[0])
+  toWrite = toWrite + '\nY-AngVel: ' + String(rocketAngVel[1])
+  toWrite = toWrite + '\nZ-AngVel: ' + String(rocketAngVel[2])
 
   writeToMemory(toWrite);
 
 }
 
-void clearMemory(){
+void clearMemory(
 
-}
+    
+
+){
